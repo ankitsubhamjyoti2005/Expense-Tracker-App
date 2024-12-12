096fe17e572b08f87d75d7aa2e0cf684d95a7ff3
ankitsubhamjyoti2005 <ankitsubhamjyoti2005@users.noreply.github.com>
2024-12-12 21:07:25 +0530

    a

--- app/src/main/java/org/example/repository/UserRepository.java
+++ app/src/main/java/org/example/repository/UserRepository.java
@@ -3,7 +3,9 @@ package org.example.repository;
 import org.example.entities.RefreshToken;
 import org.example.entities.UserInfo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserInfo, Long> {
 
     public UserInfo findByUsername(String username);
